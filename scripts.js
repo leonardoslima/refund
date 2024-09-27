f0f49ad62fe4c543dc7bb200f081131a2ab956e7
Leonardo Silva <leonardosivalima93@live.com>
2024-09-27 09:47:25 -0300

    Obtendo os dados do formulário

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -23,3 +26,7 @@ function formatCurrencyBRL(value) {
   // Retorna o valor formatado
   return value;
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
